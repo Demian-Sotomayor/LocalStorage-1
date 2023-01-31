2df20ee65a25997fd0ffa5b9cf9719aee6500724
Demian Sotomayor <demian.sotomayor.ur@gmail.com>
2023-01-31 11:51:49 -0300

    Añadiendo validación de formulario, alerta de error y setTimeOut

---
+++ 19-PROYECTO-LocalStorage/js/app.js
@@ -0,0 +1,42 @@
+/* ——————————————————————— Variables ——————————————————————— */
+const formulario = document.querySelector('#formulario');
+const listaTweets = document.querySelector('#lista-tweets');
+let tweets = [];
+
+/* ———————————————————— Event listeners ———————————————————— */
+eventListeners();
+
+function eventListeners() {
+    formulario.addEventListener('submit', agregarTweet);
+}
+
+
+/* ——————————————————————— Funciones ——————————————————————— */
+function agregarTweet(e) {
+    e.preventDefault();
+
+    /* ---- Textarea donde el usuario escribe ---- */
+    const tweet = document.querySelector('#tweet').value;
+
+    /* ---- Validación ---- */
+    if(tweet === '') {
+        mostrarError('El mensaje no puede ir vacío')
+        return; // Evita que se ejecuten más líneas de código
+    }
+}
+
+/* ---- Mostrar mensaje de error ---- */
+function mostrarError(error) {
+    const mensajeError = document.createElement('p');
+    mensajeError.textContent = error;
+    mensajeError.classList.add('error');
+    
+/* ---- Insertarlo en el contenido ---- */
+const contenido = document.querySelector('#contenido');
+contenido.appendChild(mensajeError);
+
+/* ---- Elimina la alerta tras 3 segundos ---- */
+setTimeout(() => {
+    mensajeError.remove();
+}, 3000);
+}
