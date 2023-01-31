254548f06efbb4b8479cd89636c4d11b3b7d8c7b
Demian Sotomayor <demian.sotomayor.ur@gmail.com>
2023-01-31 12:05:27 -0300

    Insertando HTML

--- 19-PROYECTO-LocalStorage/js/app.js
+++ 19-PROYECTO-LocalStorage/js/app.js
@@ -23,20 +23,58 @@ function agregarTweet(e) {
         mostrarError('El mensaje no puede ir vacío')
         return; // Evita que se ejecuten más líneas de código
     }
+
+    const tweetObj = {
+        id: Date.now(),
+        tweet,
+    }
+
+    /* ---- Añadir al arreglo de Tweets ---- */
+    tweets = [...tweets, tweetObj];
+
+    /* ---- Creando HTML ---- */
+    crearHTML();
+
+    /* ---- Reiniciar el formulario ---- */
+    formulario.reset();
 }
 
-/* ---- Mostrar mensaje de error ---- */
+    /* ---- Mostrar mensaje de error ---- */
 function mostrarError(error) {
     const mensajeError = document.createElement('p');
     mensajeError.textContent = error;
     mensajeError.classList.add('error');
     
-/* ---- Insertarlo en el contenido ---- */
-const contenido = document.querySelector('#contenido');
-contenido.appendChild(mensajeError);
+    /* ---- Insertarlo en el contenido ---- */
+    const contenido = document.querySelector('#contenido');
+    contenido.appendChild(mensajeError);
 
-/* ---- Elimina la alerta tras 3 segundos ---- */
-setTimeout(() => {
-    mensajeError.remove();
-}, 3000);
+    /* ---- Elimina la alerta tras 3 segundos ---- */
+    setTimeout(() => {
+        mensajeError.remove();
+    }, 3000);
+}
+
+/* ---- Muestra un listado de los Tweets ---- */
+function crearHTML() {
+    limpiarHTML();
+
+    if(tweets.length > 0) {
+        tweets.forEach( tweet => {
+            // Crear HTML
+
+            const li = document.createElement('li');
+            li.innerText = tweet.tweet;
+
+            // Insertarlo en el HTML
+            listaTweets.appendChild(li);
+        });
+    }
+}
+
+/* ---- Limpiar el HTML ---- */
+function limpiarHTML() {
+    while(listaTweets.firstChild) {
+        listaTweets.removeChild(listaTweets.firstChild);
+    }
 }
